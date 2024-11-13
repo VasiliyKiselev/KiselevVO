aaf4b569175d5aed2d213d2159eab4a808eb3083
VasiliyKiselev <vasakiselev19@gmail.com>
2024-11-13 16:45:40 +0300

    Исправлено Задание_2_2.py

--- Задание_2_2.py
+++ Задание_2_2.py
@@ -2,10 +2,16 @@ import math
 
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
-months = 10  # Количество месяцев, которое планируется протянуть без долгов
+months = 10  # Количество месяцев
 increase = 0.03  # Ежемесячный рост цен
 
-money_capital = 0
-for i in range(months):
-    money_capital += spend*(((increase+1)**i)) - salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+
+total_needed = 0
+
+for month in range(months):
+    deficit = spend - salary
+    if deficit > 0:
+        total_needed += deficit
+    spend *= (1 + increase)
+money_capital = math.ceil(total_needed)
+print("Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
